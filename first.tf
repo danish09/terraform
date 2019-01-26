83e22fce7585a8d91ac94428ae1586d4cd31c86c
Danish Siddiqui <danishsiddiqui09@gmail.com>
2019-01-26 09:31:09 +0000

    corrected syntax errors

--- first.tf
+++ first.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 80
     to_port     = 80
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
